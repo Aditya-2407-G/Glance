0980bb9f7f94c4d121ddfa2ace26a10665e398a5
Aditya-2407-G <adi.gupta2407@gmail.com>
2024-05-20 21:33:29 +0530

    initialization and signup page

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,9 @@
+import { z } from "zod"
+
+export const SignupValidation = z.object({
+    name: z.string().min(2, {message: "Try a longer name!"}),
+    username: z.string().min(5, {message: "Try a longer username!",}),
+    email: z.string().email(),
+    password: z.string().min(8, {message: "Password must be at least 8 characters."})
+
+  })
